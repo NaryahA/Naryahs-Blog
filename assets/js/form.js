f3299a049b7ef48e9110bdc3c6d17340ad63ad94
NaryahA <naryah335@gmail.com>
2024-11-06 03:56:05 -0500

    updated style pages and all files

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,42 @@
-EEEEEEEEEEEEEEEEEEEEEEE
+// WHEN I submit the form, THEN blog post data is stored to localStorage.
+
+
+const blogForm = document.getElementById('blog-form');
+
+
+blogForm.addEventListener('submit', function(event) {
+
+  event.preventDefault(); 
+
+  // Get form input value
+
+  const username = document.getElementById('username').value;
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+
+// WHEN I enter try to submit a form without a username, title, or content, THEN I am presented with a message that prompts me to complete the form.
+
+  if (!username || !title || !content) {
+    alert('Please complete form');
+    return;
+  }
+
+// new object for blog post with values 
+
+  const post = {
+    username,
+    title,
+    content
+  };
+
+  // blog post data is stored to localStorage
+  
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
+  
+  posts.push(post);
+  
+  localStorage.setItem('posts', JSON.stringify(posts));
+
+  window.location.href = 'blog.html';
+
+});
---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+// WHEN I click the light mode/dark mode toggle, THEN the page content's styles update to reflect the selection.
+
+const toggleMode = document.getElementById('toggle-mode');
+
+
+// I received help from a TA for this event listener function:
+
+toggleMode.addEventListener('change', function () {
+  const isChecked = this.checked;
+
+  if (isChecked === true) {
+    document.body.classList.toggle('dark-mode');
+
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+});
